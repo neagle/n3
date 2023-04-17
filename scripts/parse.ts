4dd10f82621aaaff2b5f241919f3f1db59ef1040
Nate Eagle <nate@nateeagle.com>
2023-04-17 15:18:57 -0400

    fix glob import

--- scripts/parse.ts
+++ scripts/parse.ts
@@ -1,6 +1,6 @@
 import dayjs from 'npm:dayjs@1.11.7';
 import utc from 'npm:dayjs/plugin/utc.js';
-import glob from 'https://esm.sh/glob';
+import { glob } from 'https://esm.sh/glob@10.1.0';
 import { marked } from 'npm:marked';
 import {
 	extract as frontMatter,
