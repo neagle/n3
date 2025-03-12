9a5bc692cd180f373ed6d0ecd16b76412e618693
Nate Eagle <neagle@leolabs.space>
2025-03-12 11:07:10 -0400

    fix add bookmark

--- scripts/add.ts
+++ scripts/add.ts
@@ -1,10 +1,10 @@
-import inquirer from 'npm:inquirer';
-import cheerio from 'https://esm.sh/cheerio';
-import slugify from 'https://esm.sh/@sindresorhus/slugify';
-import dayjs from 'npm:dayjs@1.11.7';
+import inquirer from 'npm:inquirer'
+import * as cheerio from 'https://esm.sh/cheerio'
+import slugify from 'https://esm.sh/@sindresorhus/slugify'
+import dayjs from 'npm:dayjs@1.11.7'
 
-let type = Deno.args[0];
-const types = ['bookmark', 'post'];
+let type = Deno.args[0]
+const types = ['bookmark', 'post']
 
 if (!type || !types.includes(type)) {
 	const answer = await inquirer.prompt([
@@ -14,12 +14,12 @@ if (!type || !types.includes(type)) {
 			message: 'What would you like to add?',
 			choices: types,
 		},
-	]);
-	type = answer.type;
+	])
+	type = answer.type
 }
 
 const create: {
-	[key: string]: () => Promise<void>;
+	[key: string]: () => Promise<void>
 } = {
 	bookmark: async () => {
 		const { url } = await inquirer.prompt([
@@ -28,29 +28,29 @@ const create: {
 				name: 'url',
 				message: 'Bookmark URL?',
 			},
-		]);
+		])
 
 		// Try to get a title and a description to serve as defaults from the
 		// bookmark page itself
 
-		let titleFromUrl;
-		let descriptionFromUrl;
+		let titleFromUrl
+		let descriptionFromUrl
 		try {
-			const response = await fetch(url);
-			const html = await response.text();
-			const $ = cheerio.load(html);
+			const response = await fetch(url)
+			const html = await response.text()
+			const $ = cheerio.load(html)
 
-			const title = $('title').text();
+			const title = $('title').text()
 			if (title) {
-				titleFromUrl = title.trim();
+				titleFromUrl = title.trim()
 			}
 
-			const description = $('meta[name="description"]').attr('content');
+			const description = $('meta[name="description"]').attr('content')
 			if (description) {
-				descriptionFromUrl = description.trim();
+				descriptionFromUrl = description.trim()
 			}
 		} catch (error) {
-			console.log(error);
+			console.log(error)
 		}
 
 		const { title } = await inquirer.prompt([
@@ -59,11 +59,10 @@ const create: {
 				default: titleFromUrl,
 				name: 'title',
 				message: 'Bookmark title?',
-				validate: (
-					input: string,
-				) => (input.length > 0 ? true : 'Title is required'),
+				validate: (input: string) =>
+					input.length > 0 ? true : 'Title is required',
 			},
-		]);
+		])
 
 		const { description, tags, source } = await inquirer.prompt([
 			{
@@ -84,7 +83,7 @@ const create: {
 				name: 'source',
 				message: 'Bookmark source?',
 			},
-		]);
+		])
 
 		const newBookmark = {
 			url,
@@ -93,31 +92,32 @@ const create: {
 			tags,
 			source,
 			date: dayjs().format('YYYY-MM-DD HH:mm:ss'),
-		};
+		}
 
-		let bookmarkFrontmatter = `---\n`;
+		let bookmarkFrontmatter = `---\n`
 		bookmarkFrontmatter += Object.entries(newBookmark)
 			.reduce((arr, [key, value]) => {
 				if (key === 'tags') {
-					arr.push(yamlList(key, value));
+					arr.push(yamlList(key, value))
 				} else {
-					arr.push(`${key}: ${value}`);
+					arr.push(`${key}: ${value}`)
 				}
-				return arr;
+				return arr
 			}, [] as string[])
-			.join('\n');
-		bookmarkFrontmatter += `\n---`;
+			.join('\n')
+		bookmarkFrontmatter += `\n---`
 
-		const filename = './src/content/bookmarks/' +
+		const filename =
+			'./src/content/bookmarks/' +
 			slugify(newBookmark.title, { lower: true, strict: true }) +
-			'.md';
+			'.md'
 		if (await exists(filename)) {
-			console.log(`File already exists with that name: ${filename}`);
+			console.log(`File already exists with that name: ${filename}`)
 		} else {
-			await Deno.writeTextFile(filename, bookmarkFrontmatter);
+			await Deno.writeTextFile(filename, bookmarkFrontmatter)
 		}
 
-		console.log(`Bookmark created: ${filename}`);
+		console.log(`Bookmark created: ${filename}`)
 	},
 	post: async () => {
 		const { title, description, tags } = await inquirer.prompt([
@@ -137,68 +137,67 @@ const create: {
 				name: 'tags',
 				message: 'Post tags?',
 			},
-		]);
+		])
 
 		const newPost = {
 			title,
 			description,
 			tags,
 			date: dayjs().format('YYYY-MM-DD HH:mm:ss'),
-		};
+		}
 
-		let frontmatter = `---\n`;
+		let frontmatter = `---\n`
 		frontmatter += Object.entries(newPost)
 			.reduce((arr, [key, value]) => {
 				if (value) {
 					if (key === 'tags') {
-						arr.push(yamlList(key, value));
+						arr.push(yamlList(key, value))
 					} else {
-						arr.push(`${key}: ${value}`);
+						arr.push(`${key}: ${value}`)
 					}
 				}
-				return arr;
+				return arr
 			}, [] as string[])
-			.join('\n');
-		frontmatter += `\n---`;
+			.join('\n')
+		frontmatter += `\n---`
 
-		const filepath = `./src/content/posts/${
-			slugify(newPost.title, { lower: true, strict: true })
-		}/`;
+		const filepath = `./src/content/posts/${slugify(newPost.title, {
+			lower: true,
+			strict: true,
+		})}/`
 
 		if (await exists(filepath)) {
-			console.log(`A post already exists with that path: ${filepath}`);
+			console.log(`A post already exists with that path: ${filepath}`)
 		} else {
-			await Deno.mkdir(filepath);
-			await Deno.writeTextFile(`${filepath}/index.md`, frontmatter);
-			console.log(`Post created: ${filepath}index.md`);
-			Deno.run({ cmd: ['code', `${filepath}index.md`] });
+			await Deno.mkdir(filepath)
+			await Deno.writeTextFile(`${filepath}/index.md`, frontmatter)
+			console.log(`Post created: ${filepath}index.md`)
+			Deno.run({ cmd: ['code', `${filepath}index.md`] })
 		}
 	},
-};
+}
 
 const exists = async (filename: string): Promise<boolean> => {
 	try {
-		await Deno.stat(filename);
+		await Deno.stat(filename)
 		// successful, file or directory must exist
-		return true;
+		return true
 	} catch (error) {
 		if (error instanceof Deno.errors.NotFound) {
 			// file or directory does not exist
-			return false;
+			return false
 		} else {
 			// unexpected error, maybe permissions, pass it along
-			throw error;
+			throw error
 		}
 	}
-};
+}
 
 const yamlList = (key: string, value: string, indentation = ''): string => {
-	return `${key}:\n- ${
-		value
-			.split(' ')
-			.map((tag) => tag.trim())
-			.join(`\n${indentation}- `)
-	}`;
-};
+	return `${key}:\n- ${value
+		.split(' ')
+		.map((tag) => tag.trim())
+		.join(`\n${indentation}- `)}`
+}
 
-create[type]();
+create[type]()
---
+++ src/content/bookmarks/hover-not-hover-sorry-not-sorry.md
@@ -0,0 +1,10 @@
+---
+url: https://nerdy.dev/hover-not-hover-sorry-not-sorry
+title: :hover > :not(:hover), sorry not sorry
+description: Great post on CSS techniques on a blog with beautiful, devvy design.
+tags:
+- css
+- design
+source: 
+date: 2025-03-12 11:04:09
+---
